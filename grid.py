f917ca233b36887e33ffe6aec651739b7cd89b51
RussellDash332 <russellsaerang@gmail.com>
2022-12-13 00:27:25 +0800

    Add grid boilerplate code

---
+++ grid.py
@@ -0,0 +1,9 @@
+m = [] # 2D array
+
+for r in range(len(m)):
+    for c in range(len(m[0])):
+        pass
+
+for dr, dc in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+    if 0 <= r+dr < len(m) and 0 <= c+dc < len(m[0]):
+        pass
