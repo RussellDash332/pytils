a9df0a3eb590ff6d883c6e23d2cbee177d338760
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2023-03-22 02:46:02 +0800

    Add prime factorization

--- sieve.py
+++ sieve.py
@@ -12,6 +12,25 @@ while p <= LIMIT:
     if p == 2: p -= 1
     p += 2
 
+# Prime factorization
+def pf(n):
+    res = {}
+    idx = 0
+    while n != 1 and idx < len(primes):
+        if n % primes[idx] == 0:
+            n //= primes[idx]
+            res[primes[idx]] = res.get(primes[idx], 0) + 1
+        else:
+            idx += 1
+    if n != 1:
+        is_prime = True
+        for p in range(primes[idx - 1], int(n**0.5) + 2):
+            if n % p == 0:
+                is_prime = False
+                res[p] = 1
+        if is_prime: res[n] = 1
+    return res
+
 # Meissel-Lehmer Algorithm
 mem = {}
 def phi(x, a):
