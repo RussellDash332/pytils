b36d97ce48a97a096d6d00f30bed04d2e9cab0e4
RussellDash332 <russellsaerang@gmail.com>
2023-07-02 10:16:23 +0800

    Add power modulo

--- nt.py
+++ nt.py
@@ -44,3 +44,13 @@ def crt(a, m, b, n):
     else:
         u, _ = bezout(m, n)
         return (a - m * u * (a - b) // d) % k
+
+# a^b % m
+def powmod(a, b, m):
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a % m
+    elif b % 2:
+        return a * powmod(a * a % m, b // 2, m) % m
+    return powmod(a * a % m, b // 2, m)
