004ae470fa2685ee502913f34e0e1445ecaaa9c2
RussellDash332 <russellsaerang@gmail.com>
2023-07-22 04:01:38 +0800

    Add alternate version of TSP

--- tsp.py
+++ tsp.py
@@ -1,5 +1,4 @@
 from itertools import combinations
-
 INF = 1e18
 def tsp(G):
     n = len(G); C = [[INF for _ in range(n)] for _ in range(1<<n)]; C[1][0] = 0
@@ -12,6 +11,18 @@ def tsp(G):
                 for j in S:
                     if j != i: C[k][i] = min(C[k][i], C[k^(1<<i)][j]+G[j][i])
     k = (1<<n)-1; return min((C[k][i]+G[i][0], i) for i in range(n))
+
+INF = 1e18
+def tsp2(G):
+    n = len(G); C = [[INF for _ in range(n)] for _ in range(1<<n)]; C[1][0] = 0
+    p = [[0 for _ in range(n)] for _ in range(1<<n)]
+    for s in range(1<<n):
+        for i in range(n):
+            for j in range(n):
+                if s&(1<<j) == 0: s2 = s+(1<<j); C[s2][j], p[s2][j] = min((C[s][i]+G[i][j], i), (C[s2][j], p[s2][j]))
+    tour = [n-1]; pos = n-1; k = (1<<n)-1
+    while pos: nxt = p[k][pos]; k -= 1<<pos; pos = nxt; tour.append(pos)
+    return tour[::-1]
 
 G = [
     [0, 20, 42, 35],
@@ -26,6 +37,12 @@ G3 = [
     [55, 3, 0]
 ]
 
+# more useful to find the cost
 print(tsp(G))
 print(tsp(G2))
 print(tsp(G3))
+
+# better to enumerate tours
+print(tsp2(G))
+print(tsp2(G2))
+print(tsp2(G3))
