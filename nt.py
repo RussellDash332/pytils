b8a2bfebed4251484d8c605320076121a5458639
RussellDash332 <russellsaerang@gmail.com>
2023-09-09 08:56:26 +0800

    Add another variant of Miller-Rabin

--- nt.py
+++ nt.py
@@ -79,6 +79,24 @@ def miller_rabin(p):
         if y != 1: return 0
     return 1
 
+# Another variant
+from random import randint
+def miller_rabin(p):
+    if p % 2 == 0: return 0
+    if p == 3: return 1
+    d, s = p-1, 0
+    while d % 2 == 0: d //= 2; s += 1
+    for _ in range(3):
+        x = pow(randint(2, p-2), d, p)
+        if x == 1 or x == p-1: continue
+        ok = 0
+        for _ in range(s):
+            x = x*x%p
+            if x == 1: return 0
+            if x == p-1: ok = 1; break
+        if not ok: return 0
+    return 1
+
 # Output a trivial factor of n, can factorize integers faster
 def pollard_rho(n):
     c = 1
