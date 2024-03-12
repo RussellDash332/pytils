b7485d27d01b674baab21a38a0e13d2396584cf5
RussellDash332 <russellsaerang@gmail.com>
2024-03-13 03:51:48 +0800

    Minor tidy-up

--- fenwick.py
+++ fenwick.py
@@ -2,13 +2,13 @@
 class FenwickTree:
     def __init__(self, arr):
         self.ft = [0]*(len(arr)+1); self.n = len(arr)
-        for i, e in enumerate(arr): self.update(i, e)
-    def update(self, idx, e):
+        for i, e in enumerate(arr): self.add(i, e)
+    def add(self, idx, e):
         idx += 1
-        while idx <= self.n: self.ft[idx] += e; idx += idx & (-idx)
+        while idx <= self.n: self.ft[idx] += e; idx += idx&(-idx)
     def get(self, idx):
         s, idx = 0, min(idx, self.n)
-        while idx > 0: s, idx = s+self.ft[idx], idx-(idx&(-idx))
+        while idx > 0: s += self.ft[idx]; idx -= idx&(-idx)
         return s
 
 # RURQ version
@@ -18,13 +18,13 @@ class FenwickTree:
         self.ft2 = [0]*(len(arr)+1)
         self.n = len(arr)
         for i in range(self.n): self.add(i, i+1, arr[i])
-    def add(self, l, r, v): # arr[l:r] += v
+    def add(self, l, r, v):
         r = min(r, self.n); p1 = p3 = l+1; p2 = p4 = r+1
         while p1 <= self.n: self.ft1[p1] += v; p1 += p1&(-p1)
         while p2 <= self.n: self.ft1[p2] -= v; p2 += p2&(-p2)
         while p3 <= self.n: self.ft2[p3] += v*l; p3 += p3&(-p3)
         while p4 <= self.n: self.ft2[p4] -= v*r; p4 += p4&(-p4)
-    def get(self, l, r): # sum(arr[l:r])
+    def get(self, l, r):
         r = min(r, self.n); s1 = s2 = s3 = s4 = 0; p1 = p2 = r; p3 = p4 = l
         while p1 > 0: s1 += self.ft1[p1]; p1 -= p1&(-p1)
         while p2 > 0: s2 += self.ft2[p2]; p2 -= p2&(-p2)
