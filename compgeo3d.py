7dffb6028bbdea24f2e010ef215d1af8f0d6f393
RussellDash332 <russellsaerang@gmail.com>
2024-04-25 16:34:44 +0800

    Optimize hypot

--- compgeo3d.py
+++ compgeo3d.py
@@ -1,6 +1,9 @@
 from math import *
 from random import *
 
+def dist(a, b, c):
+    return (a*a+b*b+c*c)**0.5
+
 def dot(a, b):
     return a[0]*b[0]+a[1]*b[1]+a[2]*b[2]
 
@@ -41,7 +44,7 @@ def chull(A):
 def proj(p, s):
     a, b = s
     d = (b[0]-a[0], b[1]-a[1], b[2]-a[2])
-    n = hypot(*d)
+    n = dist(*d)
     u = (d[0]/n, d[1]/n, d[2]/n)
     x = dot((p[0]-a[0], p[1]-a[1], p[2]-a[2]), u)
     return (a[0]+u[0]*x, a[1]+u[1]*x, a[2]+u[2]*x)
@@ -50,7 +53,7 @@ def proj(p, s):
 def proj2(p, q):
     a, b, c = q
     t = cross(a, b, c)
-    n = hypot(*t)
+    n = dist(*t)
     u = (t[0]/n, t[1]/n, t[2]/n)
     x = dot(p, u)-dot(a, u)
     return (p[0]-u[0]*x, p[1]-u[1]*x, p[2]-u[2]*x)
@@ -59,19 +62,19 @@ def proj2(p, q):
 def perp_dist(p, x):
     if len(x) == 2: q = proj(p, x)
     else: q = proj2(p, x)
-    return hypot(p[0]-q[0], p[1]-q[1], p[2]-q[2])
+    return dist(p[0]-q[0], p[1]-q[1], p[2]-q[2])
 
 # alternative method for distance from p to segment ab
 def alt_dist(p, s):
     a, b = s
-    if dot((a[0]-p[0], a[1]-p[1], a[2]-p[2]), (a[0]-b[0], a[1]-b[1], a[2]-b[2])) <= 0: return hypot(a[0]-p[0], a[1]-p[1], a[2]-p[2])
-    if dot((b[0]-p[0], b[1]-p[1], b[2]-p[2]), (b[0]-a[0], b[1]-a[1], b[2]-a[2])) <= 0: return hypot(b[0]-p[0], b[1]-p[1], b[2]-p[2])
-    return hypot(*cross(p, a, b))/hypot(a[0]-b[0], a[1]-b[1], a[2]-b[2])
+    if dot((a[0]-p[0], a[1]-p[1], a[2]-p[2]), (a[0]-b[0], a[1]-b[1], a[2]-b[2])) <= 0: return dist(a[0]-p[0], a[1]-p[1], a[2]-p[2])
+    if dot((b[0]-p[0], b[1]-p[1], b[2]-p[2]), (b[0]-a[0], b[1]-a[1], b[2]-a[2])) <= 0: return dist(b[0]-p[0], b[1]-p[1], b[2]-p[2])
+    return dist(*cross(p, a, b))/dist(a[0]-b[0], a[1]-b[1], a[2]-b[2])
 
 # line-plane intersection
 def lpx(l, p):
     c = cross(p[0], p[1], p[2])
-    n = hypot(*c)
+    n = dist(*c)
     u = (c[0]/n, c[1]/n, c[2]/n)
     x = dot(l[0], u)-dot(p[0], u)
     y = dot(l[1], u)-dot(p[0], u)
@@ -80,7 +83,7 @@ def lpx(l, p):
 def circumcenter(a, b, c):
     x = (c[0]-a[0], c[1]-a[1], c[2]-a[2])
     y = (b[0]-a[0], b[1]-a[1], b[2]-a[2])
-    s1 = hypot(*x); s2 = hypot(*y)
+    s1 = dist(*x); s2 = dist(*y)
     c = cross_util(x, y); d = (-c[0], -c[1], -c[2]); e = 2*(c[0]**2+c[1]**2+c[2]**2)
     p = cross_util(d, y); q = cross_util(c, x)
     return (a[0]+s1*s1/e*p[0]+s2*s2/e*q[0] , a[1]+s1*s1/e*p[1]+s2*s2/e*q[1], a[2]+s1*s1/e*p[2]+s2*s2/e*q[2])
@@ -89,17 +92,37 @@ def circumcenter(a, b, c):
 def mec(p):
     shuffle(p); o = p[0]; r = 0; eps = 1+1e-9
     for i in range(len(p)):
-        if hypot(o[0]-p[i][0], o[1]-p[i][1], o[2]-p[i][2]) <= r*eps: continue
+        if dist(o[0]-p[i][0], o[1]-p[i][1], o[2]-p[i][2]) <= r*eps: continue
         o = p[i]; r = 0
         for j in range(i):
-            if hypot(o[0]-p[j][0], o[1]-p[j][1], o[2]-p[j][2]) <= r*eps: continue
+            if dist(o[0]-p[j][0], o[1]-p[j][1], o[2]-p[j][2]) <= r*eps: continue
             o = ((p[i][0]+p[j][0])/2, (p[i][1]+p[j][1])/2, (p[i][2]+p[j][2])/2)
-            r = hypot(o[0]-p[i][0], o[1]-p[i][1], o[2]-p[i][2])
+            r = dist(o[0]-p[i][0], o[1]-p[i][1], o[2]-p[i][2])
             for k in range(j):
-                if hypot(o[0]-p[k][0], o[1]-p[k][1], o[2]-p[k][2]) <= r*eps: continue
+                if dist(o[0]-p[k][0], o[1]-p[k][1], o[2]-p[k][2]) <= r*eps: continue
                 o = circumcenter(p[i], p[j], p[k])
-                r = hypot(o[0]-p[i][0], o[1]-p[i][1], o[2]-p[i][2])
+                r = dist(o[0]-p[i][0], o[1]-p[i][1], o[2]-p[i][2])
     return o, r
+
+def mec2(p):
+    def helper(n, k):
+        if k >= 3:
+            c = circumcenter(p[n-1], p[n-2], p[n-3])
+            r = dist(c[0]-p[n-1][0], c[1]-p[n-1][1], c[2]-p[n-1][2])
+            print(c, r)
+            return c, r
+        if n == 1: return p[0], 0
+        if n == 2: return ((p[0][0]+p[1][0])/2, (p[0][1]+p[1][1])/2, (p[0][2]+p[1][2])/2), dist(p[0][0]-p[1][0], p[0][1]-p[1][1], p[0][2]-p[1][2])/2
+        i = randint(0, n-k-1)
+        p[i], p[n-1-k] = p[n-1-k], p[i]; p[n-1-k], p[n-1] = p[n-1], p[n-1-k]
+        o, r = helper(n-1, k)
+        p[n-1-k], p[n-1] = p[n-1], p[n-1-k]; p[i], p[n-1-k] = p[n-1-k], p[i]
+        if dist(p[i][0]-o[0], p[i][1]-o[1], p[i][2]-o[2]) <= r+1e-9: return o, r
+        p[i], p[n-1-k] = p[n-1-k], p[i]
+        x = helper(n, k+1)
+        p[i], p[n-1-k] = p[n-1-k], p[i]
+        return x
+    return helper(len(p), 0)
 
 if __name__ == '__main__':
     P = [(1, 0, 0), (1, 1, 0), (0, 0, 0), (0, 0, 1)]
@@ -111,11 +134,11 @@ if __name__ == '__main__':
     print(alt_dist((4, 2, 1), ((1, 0, 1), (1, 2, 0)))) # sqrt(9.8)
     print(perp_dist((4, 2, 1), ((1, 0, 1), (1, 2, 0)))) # sqrt(9.8)
     x, y, z = proj((4, 2, 1), ((1, 0, 1), (1, 2, 0)))
-    print(hypot(4-x, 2-y, 1-z))
+    print(dist(4-x, 2-y, 1-z))
 
     print(perp_dist((1, 1, 1), ((0, 0, 0), (-1, 2, -1), (5, 2, -3)))) # sqrt(126)/7
     x, y, z = proj2((1, 1, 1), ((0, 0, 0), (-1, 2, -1), (5, 2, -3)))
-    print(hypot(1-x, 1-y, 1-z))
+    print(dist(1-x, 1-y, 1-z))
 
     print((x, y, z))
     print(lpx(((1, 1, 1), (-2, -5, -8)), ((0, 0, 0), (-1, 2, -1), (5, 2, -3))))
@@ -123,3 +146,4 @@ if __name__ == '__main__':
     print(circumcenter((3, 2, -5), (-3, 8, -5), (-3, 2, 1)))
 
     print(mec([(3, 2, -5), (-3, 8, -5), (-3, 2, 1)]))
+    print(mec2([(3, 2, -5), (-3, 8, -5), (-3, 2, 1)]))
