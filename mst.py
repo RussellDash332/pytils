656a2311b7cfe65afd89dfec87b4cf4f060a34d5
RussellDash332 <russellsaerang@gmail.com>
2024-05-15 16:50:22 +0800

    Fix MST bug

--- mst.py
+++ mst.py
@@ -23,7 +23,7 @@ while len(mst) != n:
     best = (0, A[0])
     for i in range(1, n):
         if A[i] < best[1]: best = (i, A[i])
-    mst.add(best)
+    mst.append(best)
     A[best[0]], B[best[0]] = INF, 1
     for v in g[best[0]]:
         if not B[v] and A[v] > g[best[0]][v]: A[v] = g[best[0]][v]
