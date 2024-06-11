39190906e72a211bca68aeabb605bef9c3eb29ab
RussellDash332 <russellsaerang@gmail.com>
2024-06-11 17:45:04 +0800

    Update Manacher's algorithm

--- manacher_subpalindromes.py
+++ manacher_subpalindromes.py
@@ -1,15 +1,10 @@
-S = 'aabacab'; N = len(S)
+S = 'aabacab'
 
-s = '@#'+'#'.join(S)+'#^'
-n = len(s)-2; p = [0]*(n+2); l = r = 1
-for i in range(n+1):
+N = len(S); s = '@#'+'#'.join(S)+'#^'; p = [0]*(2*N+3); l = r = 1
+for i in range(2*N+2):
     p[i] = max(0, min(r-i, p[l+r-i]))
     while s[i-p[i]] == s[i+p[i]]: p[i] += 1
     if i+p[i] > r: l = i-p[i]; r = i+p[i]
-do = [0]*N; de = [0]*N
-for i in range(1, 2*N, 2): de[i//2] = p[i]//2
-for i in range(2, 2*N+1, 2): do[i//2-1] = p[i]//2
 
-print(S)
-print('Odd-length:', do)
-print('Even-length:', de)
+print('Odd-length:', [p[2*i+2]//2 for i in range(N)]) # index i = number of subpalindromes centered at s[i]
+print('Even-length:', [p[2*i+1]//2 for i in range(N)]) # index i(>0) = number of subpalindromes centered at s[i-1]+s[i]
