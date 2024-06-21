b7e2b36348186d9ecc89effc89a9576c7e286f8d
RussellDash332 <russellsaerang@gmail.com>
2024-06-21 18:49:56 +0700

    Add PR+Brent modification

--- number_theory_pollard_rho.py
+++ number_theory_pollard_rho.py
@@ -8,5 +8,26 @@ def pollard_rho(n):
         if d != n: return d
         c += 1
 
+# With Brent modification
+# Credits: Brandon Tang
+from math import gcd; from random import *
+def pollard_rho_brent(n):
+    while True:
+        y = randint(1, n-1); c = randint(1, n-1); m = randint(1, n-1); g = r = q = 1
+        while g == 1:
+            x = y; k = 0
+            for _ in range(r): y = (y*y+c)%n
+            while k < r and g == 1:
+                s = y
+                for _ in range(min(m, r-k)): y = (y*y+c)%n; q = q*abs(x-y)%n
+                g = gcd(q, n); k += m
+            r *= 2
+        if g == n: g = 1
+        while g == 1: s = (s*s+c)%n; g = gcd(abs(x-s), n)
+        if g != n: return g
+
 print(pollard_rho(105))
 print(pollard_rho(10**9+3)) # not a prime
+
+print(pollard_rho_brent(105))
+print(pollard_rho_brent(10**9+3)) # not a prime
