ae230f3ce4a7f87896d7754d6ae9ccf95445dd49
RussellDash332 <russellsaerang@gmail.com>
2024-06-21 17:33:44 +0700

    Update Pollard Rho

--- number_theory_pollard_rho.py
+++ number_theory_pollard_rho.py
@@ -4,12 +4,9 @@ def pollard_rho(n):
     c = 1
     while True:
         x, y, d = 2, 2, 1
-        while d == 1: 
-            x = (x*x+c)%n
-            y = (y*y+c)%n; y = (y*y+c)%n
-            d = gcd(abs(x-y), n)
+        while d == 1: x = (x*x+c)%n; y = (y*y+c)%n; y = (y*y+c)%n; d = gcd(abs(x-y), n)
         if d != n: return d
-        else: c += 1
+        c += 1
 
 print(pollard_rho(105))
 print(pollard_rho(10**9+3)) # not a prime
