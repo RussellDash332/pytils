97c3b4e19de2fe5c12c0641ab87cac6fb2c9c15e
RussellDash332 <russellsaerang@gmail.com>
2026-01-28 00:30:25 +0800

    Add RUPQ variant of Fenwick Tree

--- fenwick_tree_rsq.py
+++ fenwick_tree_rsq.py
@@ -9,6 +9,22 @@ class FenwickTree:
     def get(self, idx):
         s, idx = 0, min(idx, self.n)
         while idx > 0: s += self.ft[idx]; idx -= idx&(-idx)
+        return 
+
+# RUPQ version
+class FenwickTree:
+    def __init__(self, arr):
+        self.ft = [0]*(len(arr)+1)
+        self.n = len(arr)
+        for i in range(self.n): self.add(i, i+1, arr[i])
+    def _add(self, idx, e):
+        idx += 1
+        while idx <= self.n: self.ft[idx] += e; idx += idx&(-idx)
+    def add(self, l, r, e): # a[l:r] += e
+        self._add(l, e); self._add(min(r, self.n), -e)
+    def get(self, idx): # a[idx]
+        s, idx = 0, min(idx+1, self.n)
+        while idx > 0: s += self.ft[idx]; idx -= idx&(-idx)
         return s
 
 # RURQ version
