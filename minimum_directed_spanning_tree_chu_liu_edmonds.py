bd29392a26e28110ecf1a2da345c8aab87694efb
RussellDash332 <russellsaerang@gmail.com>
2026-02-12 21:10:58 +0800

    No to recursion, no to Node OOP

--- minimum_directed_spanning_tree_chu_liu_edmonds.py
+++ minimum_directed_spanning_tree_chu_liu_edmonds.py
@@ -3,7 +3,6 @@ N = 4
 G = [{1: 40, 2: 95, 3: 95}, {1: 1, 2: 95, 3: 50}, {1: 20, 2: 95, 3: 1}, {1: 10, 2: 1, 3: 20}]
 root = 0
 _RECONSTRUCT_TREE = True
-_BAD = False
 
 class UFDS:
     def __init__(s, N):
@@ -30,39 +29,43 @@ class UFDSRB:
             return 1
     def undo(s):
         if s.o: v, x = s.o.pop(); s.p[s.p[v]] -= x; s.p[v] = x
-class Node:
-    def __init__(s, k):
-        s.k = k; s.l = s.r = None; s.d = 0
-    def prop(s):
-        s.k[0] += s.d
-        if s.l: s.l.d += s.d
-        if s.r: s.r.d += s.d
-        s.d = 0
-    def top(s):
-        s.prop(); return s.k
+def prop(s):
+    if ~L[s]: D[L[s]] += D[s]
+    if ~R[s]: D[R[s]] += D[s]
+    W[s] += D[s]; D[s] = 0
 def merge(a, b):
-    if not a or not b: return a or b
-    a.prop(); b.prop()
-    if a.k[0] > b.k[0]: a, b = b, a
-    a.l, a.r = merge(b, a.r), a.l; return a
-def pop(a): a.prop(); return merge(a.l, a.r)
-E = []; H = [None]*N; Z = 0; S = [-1]*N; S[root] = root; R = [0]*N; Q = [None]*N
+    S = [(a, b, 0)]; T = []
+    while S:
+        a, b, p = S.pop()
+        if p: L[a], R[a] = T.pop(), L[a]; T.append(a)
+        else:
+            if a<0 or b<0: T.append(a if ~a else b)
+            else:
+                prop(a); prop(b)
+                if W[a] > W[b]: a, b = b, a
+                S.append((a, b, 1)); S.append((b, R[a], 0))
+    return T.pop()
+def top(s): prop(s); return (W[s], A[s], B[s])
+def pop(a): prop(a); return merge(L[a], R[a])
+_BAD = False; H = [-1]*N; Z = 0; S = [-1]*N; S[root] = root; Y = [0]*N
+L = []; R = []; D = []; W = []; A = []; B = []
 for i in range(N):
-    for j in G[i]: E.append((G[i][j], i, j))
-for w, a, b in E: H[b] = merge(H[b], Node([w, a, b]))
-if _RECONSTRUCT_TREE: I = [[-1]*2]*N; C = []; P = [-1]*N; U = UFDSRB(N)
+    for j in G[i]: W.append(G[i][j]); A.append(i); B.append(j); L.append(-1); R.append(-1); D.append(0); H[j] = merge(H[j], len(W)-1)
+if _RECONSTRUCT_TREE: I = [[-1]*2]*N; C = []; P = [-1]*N; U = UFDSRB(N); Q = [None]*N
 else: U = UFDS(N)
 for s in range(N):
     u = s; q = 0; w = 0
     if _BAD: break
     while S[u] < 0:
-        if not H[u]: print('No such MST'); _BAD = True; break
-        e = H[u].top(); Z += e[0]; H[u].d -= e[0]; H[u] = pop(H[u]); Q[q] = e; R[q] = u; q += 1; S[u] = s; u = U.find(e[1])
+        if H[u]<0: print('No such MST'); _BAD = True; break
+        e = top(H[u]); Z += e[0]; D[H[u]] -= e[0]; H[u] = pop(H[u]); Y[q] = u
+        if _RECONSTRUCT_TREE: Q[q] = e
+        q += 1; S[u] = s; u = U.find(e[1])
         if S[u] == s:
-            c = None; E = q
-            if _RECONSTRUCT_TREE: T = len(U.o)
+            c = -1
+            if _RECONSTRUCT_TREE: E = q; T = len(U.o)
             while 1:
-                c = merge(c, H[w:=R[q:=q-1]])
+                c = merge(c, H[w:=Y[q:=q-1]])
                 if not U.union(u, w): break
             H[u:=U.find(u)] = c; S[u] = -1
             if _RECONSTRUCT_TREE: C.append((u, T, Q[q:E]))
@@ -78,5 +81,6 @@ if _RECONSTRUCT_TREE and not _BAD:
         if I[i]: P[i] = I[i][1]
 
 if __name__ == '__main__':
-    print('MDST length:', Z)
-    if _RECONSTRUCT_TREE: print('Parent:', P)
+    if not _BAD:
+        print('MDST length:', Z)
+        if _RECONSTRUCT_TREE: print('Parent:', P)
