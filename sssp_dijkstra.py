4657f21a1c53f5701110ec08cb9c41f416bccb4e
RussellDash332 <russellsaerang@gmail.com>
2026-07-20 00:07:15 +0800

    Change type of adjacency list

--- sssp_dijkstra.py
+++ sssp_dijkstra.py
@@ -2,15 +2,15 @@ from heapq import *
 
 n = 2 # number of vertices
 s, t = 0, 1 # source and destination
-g = [{} for _ in range(n)]
+G = [[] for _ in range(n)]
 
 # Simple version, works like a charm + customizable for state-space Dijkstra
 INF = float('inf'); D = [INF]*n; D[s] = 0; pq = [(0, s)]
 while pq:
     dd, vv = heappop(pq)
     if dd != D[vv]: continue
-    for nn in g[vv]:
-        if D[nn] > (new:=dd+g[vv][nn]): D[nn] = new; heappush(pq, (new, nn))
+    for nn, w in G[vv]:
+        if D[nn] > (new:=dd+w): D[nn] = new; heappush(pq, (new, nn))
 print(D[t])
 
 # Optimized version - coordinate compression + array.array
@@ -19,6 +19,6 @@ INF = 10**9; D = array('i', [INF]*n); D[s] = 0; pq = [s]
 while pq:
     dv = heappop(pq); dd, vv = dv//n, dv%n
     if dd != D[vv]: continue
-    for nn in g[vv]:
-        if D[nn] > (new:=dd+g[vv][nn]): D[nn] = new; heappush(pq, new*n+nn)
+    for nn, w in G[vv]:
+        if D[nn] > (new:=dd+w): D[nn] = new; heappush(pq, new*n+nn)
 print(D[t])
