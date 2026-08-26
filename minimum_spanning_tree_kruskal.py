3178be12869917f4c096fb052451f19c32b3012e
RussellDash332 <russellsaerang@gmail.com>
2026-08-27 00:55:49 +0800

    Update UFDS in Kruskal

--- minimum_spanning_tree_kruskal.py
+++ minimum_spanning_tree_kruskal.py
@@ -3,16 +3,16 @@ g = [{} for _ in range(n)]
 
 # Kruskal's algorithm
 class UFDS:
-    def __init__(self, N):
-        self.p = [*range(N)]; self.rank = [0]*N
-    def find(self, i):
-        if self.p[i] == i: return i
-        self.p[i] = self.find(self.p[i])
-        return self.p[i]
-    def union(self, i, j):
-        if (x:=self.find(i)) != (y:=self.find(j)):
-            if self.rank[x] > self.rank[y]: self.p[y] = x
-            else: self.p[x] = y; self.rank[y] += self.rank[x] == self.rank[y]
+    def __init__(s, N):
+        s.p = [*range(N)]; s.r = [0]*N
+    def find(s, i):
+        if s.p[i] == i: return i
+        s.p[i] = s.find(s.p[i])
+        return s.p[i]
+    def union(s, i, j):
+        if (x:=s.find(i)) != (y:=s.find(j)):
+            if s.r[x] > s.r[y]: s.p[y] = x
+            else: s.p[x] = y; s.r[y] += s.r[x] == s.r[y]
 el = []; mst = []; u = UFDS(n)
 for a in range(n):
     for b in g[a]: el.append((g[a][b], a, b))
